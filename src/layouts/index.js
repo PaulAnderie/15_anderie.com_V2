570a9ff808823f8a0a3d82f85bde93b2fdb2182e
OStap <ostap.adamchuk@hexup.co>
2019-08-19 14:52:44 +0300

    added Schema Markup and font semiBold

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,6 +17,26 @@ const TemplateWrapper = ({ children,  data }) => (
       <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"/>
       <script type="text/javascript" src="https://www.provenexpert.com/widget/landing_prandible-ehrliches-online-training.js?feedback=1&avatar=0&competence=1&style=white"/>
       <link rel="stylesheet" type="text/css" href="https://www.provenexpert.com/css/widget_landing.css" media="screen,print"/>
+    <script type="application/ld+json">{`
+      {
+        "@context": "https://schema.org/",
+        "@type": "Person",
+        "name": "Paul Anderie",
+        "url": "https://anderie.com",
+        "image": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj7nJ7Ato7kAhUFPVAKHfsDDzIQjRx6BAgBEAQ&url=https%3A%2F%2Ftwitter.com%2Fpaulanderie&psig=AOvVaw3HMBACaHtwbwGk_ehukQU1&ust=1566286623820876",
+        "sameAs": [
+          "https://www.linkedin.com/in/paulanderie/",
+          "https://www.instagram.com/paulanderie/",
+          "https://twitter.com/paulanderie",
+          "https://www.facebook.com/paul.anderie"
+        ],
+        "jobTitle": "Founder and Entrepreneur",
+        "worksFor": {
+          "@type": "Organization",
+          "name": "prandible"
+        }
+      }
+  `}</script>
     </Helmet>
     {children()}
   </div>
