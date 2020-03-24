c40a97b85099145816c5f56460dc503852127671
Florian Hülsmann <fh@cbix.de>
2020-03-24 15:30:26 +0100

    added gatsby-plugin-google-tagmanager

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,18 +5,18 @@ import './index.css'
 
 const TemplateWrapper = ({ children,  data }) => (
 
-  <div>
+    <div>
     <Helmet
-      title="Paul Anderie - Digital Marketing Trainer & Consultant"
-      meta={[
+    title="Paul Anderie - Digital Marketing Trainer & Consultant"
+    meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
-      ]}
+    ]}
     >
-      <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
-      <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"/>
-      <script type="text/javascript" src="https://www.provenexpert.com/widget/landing_prandible-ehrliches-online-training.js?feedback=1&avatar=0&competence=1&style=white"/>
-      <link rel="stylesheet" type="text/css" href="https://www.provenexpert.com/css/widget_landing.css" media="screen,print"/>
+    <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
+    <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"/>
+    <script type="text/javascript" src="https://www.provenexpert.com/widget/landing_prandible-ehrliches-online-training.js?feedback=1&avatar=0&competence=1&style=white"/>
+    <link rel="stylesheet" type="text/css" href="https://www.provenexpert.com/css/widget_landing.css" media="screen,print"/>
     <script type="application/ld+json">{`
       {
         "@context": "https://schema.org/",
@@ -39,7 +39,7 @@ const TemplateWrapper = ({ children,  data }) => (
   `}</script>
     </Helmet>
     {children()}
-  </div>
+    </div>
 )
 
 
